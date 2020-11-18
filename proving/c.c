09e6e609f6e4d61debeed37729c5659febec85fe
Kai Tindall <kaitindall2000@gmail.com>
2020-11-18 16:21:48 +0000

    Main tests working

---
+++ proving/c.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int _by;int main(int argc, char* argv[]) {
+int a;for (a = ((1)); _by = ((1)), (a-((13)))*((_by > 0) - (_by < 0)) <= 0;a+= _by) {if (!(((a))==((7)))) {printf("%d", a);printf("\n");}}a = (0);do {a = ((a)) + ((1));if ((!(((a))==((6))))&&(((a))!=((8)))) {printf("%d", a);printf("\n");}} while(((a))<((14)));a = (0);while (((a))<((12))) {printf("%d", a);a = ((a)) + ((1));}printf("\n");
+return 0; 
+}
